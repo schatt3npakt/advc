0590e60b245f723f3385c41b4f3a0ed104e0c814
Luca La Furia <dev@bitheart.de>
2023-03-04 12:00:17 +0100

    implement multiple puzzle loading

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,17 +1,13 @@
 import { defineStore } from "pinia";
 import { ref } from "vue"
+import allPuzzles from "../data/puzzles.json"
 
 export const useStore = defineStore("store", () => {
-  const puzzleConfig = ref({
-    completionText: "Lorem Ipsum",
-    initText: "Lorem Ipsum",
-    maxInputs: 10,
-    rows: [
-      ["UP", "DOWN", "LEFT", "RIGHT", "RIGHT", "DOWN"]
-    ],
-    sorting: "3124",
-    solution: "UPDOWNLEFTRIGHTRIGHTDOWN",
+  const appState = ref({
+    currentPuzzleIndex: 0
   })
+  let puzzleConfig = ref(null)
+  puzzleConfig.value = allPuzzles[appState.value.currentPuzzleIndex]
 
   const initialPuzzleState = {
     isPuzzleSolved: false,
@@ -40,6 +36,14 @@ export const useStore = defineStore("store", () => {
         puzzleState.value.isPuzzleSolved = false
         puzzleState.value.numberOfInputs = 0
         puzzleState.value.state = []
+
+        if ((appState.value.currentPuzzleIndex + 1) < allPuzzles.length) {
+          appState.value.currentPuzzleIndex++
+        } else {
+          appState.value.currentPuzzleIndex = 0
+        }
+
+        puzzleConfig.value = allPuzzles[appState.value.currentPuzzleIndex]
       }, 1000)
     }
   }
